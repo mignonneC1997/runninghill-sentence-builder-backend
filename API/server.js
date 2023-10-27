2a02d691edf5c6fa47dfdad6f97ab903d891f9c0
mignonnecilliers <mignonne@softwareworx.co.za>
2023-10-27 17:31:47 +0200

    #01 added http get sentences method

--- API/server.js
+++ API/server.js
@@ -15,6 +15,35 @@ const connectDB = async () => {
 
 connectDB();
 
+async function getCollectionData(type) {
+    try {
+        // Define the schema for your model
+        const wordSchema = new mongoose.Schema({ word: String });
+        const collectionModel = mongoose.models[type] || mongoose.model(type, wordSchema);
+      // Ensure the collection has an index for better performance
+        await collectionModel.ensureIndexes();
+    
+        const data = await collectionModel.find({}).maxTimeMS(60000); // 60 seconds timeout
+    
+        return data;
+        } catch (error) {
+            console.error('Error retrieving data:', error);
+            logger.error(`mongoose get data ERROR - ${error.message}`);
+            connectDB();
+        }
+}
+
+app.get('/sentences', async (req, res) => {
+    try {
+        const data = await getCollectionData('sentences');
+        res.json({ recordset: data });
+    } catch (error) {
+        console.error('Error in calling getCollectionData:', error);
+        logger.error(`get sentences ERROR - ${error.message}`);
+        connectDB();
+    }
+});
+
 // Start the Express server
 app.listen(serverPort, () => {
     console.log(`Server is running on port ${serverPort}`);
