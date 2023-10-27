33c52ed32e6575de7981f20713662023c641af26
mignonnecilliers <mignonne@softwareworx.co.za>
2023-10-27 16:00:18 +0200

    #01 added mongodb config

--- API/config.js
+++ API/config.js
@@ -1,5 +1,7 @@
 const serverPort = process.env.PORT || 5519;  
+const dbConnectUrl = 'mongodb+srv://testUser:testing123@cluster0.3maq2o9.mongodb.net/?retryWrites=true&w=majority';
 
 module.exports = {
-    serverPort
+    serverPort,
+    dbConnectUrl
 }
