efeb8b1891d2ada1ec00584ea348ce36c889ab43
mignonnecilliers <mignonne@softwareworx.co.za>
2023-10-27 19:40:34 +0200

    #03 added http method for words and sentences

--- API/server.js
+++ API/server.js
@@ -15,6 +15,50 @@ const connectDB = async () => {
 
 connectDB();
 
+async function getCollectionData(type) {
+    try {
+        // Define the schema for your model
+        const wordSchema = new mongoose.Schema({ word: String });
+        const collectionModel = mongoose.models[type] || mongoose.model(type, wordSchema);
+      // Ensure the collection has an index for better performance
+        await collectionModel.ensureIndexes();
+    
+        const data = await collectionModel.find({}).maxTimeMS(60000); // 60 seconds timeout
+    
+        return data;
+        } catch (error) {
+            console.error('Error retrieving data:', error);
+            logger.error(`mongoose get data ERROR - ${error.message}`);
+            connectDB();
+        }
+}
+
+
+app.get('/sentences', async (req, res) => {
+    try {
+        const data = await getCollectionData('sentences');
+        res.json({ recordset: data });
+    } catch (error) {
+        console.error('Error in calling getCollectionData:', error);
+        logger.error(`get sentences ERROR - ${error.message}`);
+        connectDB();
+    }
+});
+
+app.get('/wordTypes', async (req, res, next) => {
+    try {
+        const data = await getCollectionData('words');
+        console.log('dat == ', data);
+        res.status(200).json({
+            response: data,
+            api: 'success',
+        });
+    } catch (err) {
+        connectDB();
+        next(err);
+    }
+});
+
 // Start the Express server
 app.listen(serverPort, () => {
     console.log(`Server is running on port ${serverPort}`);
