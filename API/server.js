500ad169e283e839cad437d4b3de2450f85d8f59
mignonnecilliers <mignonne@softwareworx.co.za>
2023-10-27 16:02:13 +0200

    #01 connected to mongoDB

--- API/server.js
+++ API/server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-const { serverPort } = require('./config');
+const { serverPort, dbConnectUrl } = require('./config');
+
+const connectDB = async () => {
+    mongoose.connect(dbConnectUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    }).catch((err) => {
+        logger.error(`MongoDB CONNECT ERROR - ${err.message}`);
+    });
+}
+
+connectDB();
 
 // Start the Express server
 app.listen(serverPort, () => {
     console.log(`Server is running on port ${serverPort}`);
 });
+
